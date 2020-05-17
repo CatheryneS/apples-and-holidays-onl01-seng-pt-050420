f7c3df24b40266f4dbf75c6377c74750e5162057
Catheryne Small <catheryn.small@gmail.com>
2020-05-17 16:34:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday_and_supplies|
     holiday_and_supplies.to_a.each do |holiday, supply|
-      supply.select == "BBQ"
+      
     end
   end
 end
